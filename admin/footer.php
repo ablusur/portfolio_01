f7f9cf8a4aa449b0e81fe3299930fa9dd9cba8c3
ablusur <110009138+ablusur@users.noreply.github.com>
2023-06-12 19:18:26 +0600

    header and footer secti divide from admin/home.php

---
+++ admin/footer.php
@@ -0,0 +1,15 @@
+
+
+</div>
+    </div>
+
+    <!--footer copyright section -->
+    <div
+      class="container-fluid text-center"
+      id="footerDiv"
+      style="height: 10%; margin-top: 50px"
+    >
+      <p><b>copyright &copy; eShikhon.com 2023</b></p>
+    </div>
+  </body>
+</html>
--- admin/home.php
+++ admin/home.php
@@ -1,54 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>homepage</title>
 
-    <link rel="stylesheet" href="style.css" />
 
-    <!-- Latest compiled and minified CSS -->
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    />
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-    <!-- Header div section -->
-
-    <div class="container-fluid text-center" id="hDiv" style="height: 100px">
-      <h1><b>A D M I N &nbsp;&nbsp; P A N E L</b></h1>
-    </div>
-
-    <!-- Body main container section -->
-
-    <div class="container-fluid" style="height: 80%; padding: 0">
-      <div class="row">
-        <!-- Navigation div section -->
-
-        <div class="col-md-3" id="navDiv">
-          <h4 id="title">CONTROLS</h4>
-          <ul id="navList">
-            <li><a href="">Admin Home</a></li>
-            <li><a href="">Edit Profile</a></li>
-            <li><a href="">Change Password</a></li>
-            <li><a href="">Manage Admin</a></li>
-            <li><a href="">Manage Employee</a></li>
-            <li><a href="">Add News</a></li>
-            <li><a href="">Add Videos</a></li>
-            <li><a href="">Manage Videos</a></li>
-            <li><a href="">Show Feedbacks</a></li>
-            <li><a href="">Total Subscribers</a></li>
-            <li><a href="">Log Out</a></li>
-          </ul>
-        </div>
+  <?php include('header.php'); ?>
 
         <!--  content div section -->
 
@@ -56,16 +8,7 @@
           <h2><b>Welcome to </b></h2>
           <img src="../images/logo-full-final.png" alt="photo" width="50%" />
         </div>
-      </div>
-    </div>
 
-    <!--footer copyright section -->
-    <div
-      class="container-fluid text-center"
-      id="footerDiv"
-      style="height: 10%; margin-top: 50px"
-    >
-      <p><b>copyright &copy; eShikhon.com 2023</b></p>
-    </div>
-  </body>
-</html>
+
+
+  <?php include('footer.php');?>
