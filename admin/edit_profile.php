4493dcbfe9060a6d89a28d2d47d4e52168c068ad
ablusur <110009138+ablusur@users.noreply.github.com>
2023-06-13 13:34:53 +0600

    input set

--- admin/edit_profile.php
+++ admin/edit_profile.php
@@ -1,6 +1,25 @@
 <?php include('header.php'); ?>
 
+<!-- Content div section -->
 
+<div class="col-md-9">
+  <h4>Edit Your Information</h4>
 
+  <form class="form-horizontal" action="" method="" style="width: 80%"></form>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="">Name:</label>
+
+    <div class="col-sm-10">
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        name="name"
+        value="Ablusur Rahman Dihider"
+      />
+    </div>
+  </div>
+</div>
 
 <?php include('footer.php');?>
